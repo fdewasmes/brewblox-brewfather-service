6309674580f180610449e8198fb0809d04c0cfa0
Fabrice Dewasmes <fabrice.dewasmes@smile.fr>
2021-06-24 15:10:32 +0200

    added timer management for rest

--- brewblox_brewfather_service/brewfather_automation.py
+++ brewblox_brewfather_service/brewfather_automation.py
@@ -3,11 +3,15 @@ A Brewfather API client
 """
 
 import asyncio
+from datetime import datetime, timedelta
+
 from aiohttp import web
 from aiohttp_apispec import docs
 from brewblox_service import brewblox_logger, features, mqtt
-from brewblox_brewfather_service.datastore import (ConfigurationDatastore,
-                                                   Device, MashAutomation, CurrentState, Settings, DatastoreClient)
+
+from brewblox_brewfather_service.schemas import (AutomationState, AutomationType,
+                                                 Device, MashAutomation, CurrentState, Settings)
+from brewblox_brewfather_service.datastore import DatastoreClient
 from brewblox_brewfather_service import schemas
 from brewblox_brewfather_service.api.brewfather_api_client import BrewfatherClient
 from brewblox_spark_api.blocks_api import BlocksApi
@@ -30,7 +34,10 @@ class BrewfatherFeature(features.ServiceFeature):
         self.datastore_client = DatastoreClient(self.app)
 
         # TODO get these from service configuration
-        self.setpoint_device = Device('spark-one', 'HERMS MT Setpoint')
+        setpoint_device = Device('spark-one', 'HERMS MT Setpoint')
+        self.settings = Settings(MashAutomation(setpoint_device))
+        await self.datastore_client.store_settings(self.settings)
+
         self.spark_client.on_blocks_change(self.spark_blocks_changed)
 
         await mqtt.listen(app, 'brewcast/state/#', self.on_message)
@@ -57,40 +64,40 @@ class BrewfatherFeature(features.ServiceFeature):
         if number_of_steps == 0:
             raise ValueError
 
-        configuration = ConfigurationDatastore(
-            Settings(MashAutomation(self.setpoint_device)),
-            CurrentState('mash', -1),
-            mash)
-
-        await self.datastore_client.store_configuration(configuration)
+        await self.datastore_client.store_mash_steps(mash_data)
 
     async def start_automated_mash(self):
-        self.proceed_to_next_step(self)
+        state = CurrentState(AutomationType.MASH, -1, None)
+        await self.datastore_client.store_state(state)
+        await self.proceed_to_next_step()
 
     async def proceed_to_next_step(self):
-        configuration = await self.datastore_client.load_configuration()
 
-        LOGGER.info(f'current state: {configuration.current_state}')
-        current_step = configuration.current_state.step
+        mash = await self.datastore_client.load_mash()
+        state = await self.datastore_client.load_state()
 
+        LOGGER.info(f'Proceeding to next step from current state: {state}')
+        current_step = state.step_index
         current_step += 1
         try:
-
-            target_temp = configuration.mash.steps[current_step].stepTemp
-            LOGGER.info(target_temp)
+            step = mash.steps[current_step]
+            state.step = step
+            state.step_index = current_step
+            state.automation_state = AutomationState.HEAT
+            target_temp = step.stepTemp
             await self.adjust_mash_setpoint(target_temp)
-            configuration.current_state.step = current_step
-            LOGGER.info(f'current state: {configuration.current_state}')
-            await self.datastore_client.store_configuration(configuration)
+            LOGGER.info(f'current state: {state}')
+            await self.datastore_client.store_state(state)
         except IndexError:
-            LOGGER.warn('current recipe has no mash steps')
+            LOGGER.warn('current recipe has no more mash steps')
 
     async def adjust_mash_setpoint(self, target_temp):
         try:
-            block = await asyncio.wait_for(self.spark_client.read(self.setpoint_device.id), timeout=5.0)
+            block = await asyncio.wait_for(self.spark_client.read(self.settings.mashAutomation.setpointDevice.id),
+                                           timeout=5.0)
             previous_temp = block['data']['storedSetting']['value']
             block['data']['storedSetting']['value'] = target_temp
-            returned_block = await asyncio.wait_for(self.spark_client.patch(self.setpoint_device.id, block['data']),
+            returned_block = await asyncio.wait_for(self.spark_client.patch(self.settings.mashAutomation.setpointDevice.id, block['data']),
                                                     timeout=5.0)
             new_temp = returned_block['data']['storedSetting']['value']
             LOGGER.info(f'mash setpoint changed from {previous_temp} to {new_temp}')
@@ -105,19 +112,33 @@ class BrewfatherFeature(features.ServiceFeature):
     async def on_message(self, topic: str, message: dict):
         """ nothing to do yet"""
 
+    async def start_timer(self):
+        state = await self.datastore_client.load_state()
+        state.automation_state = AutomationState.REST
+        state.step_start_time = datetime.utcnow()
+        state.step_end_time = state.step_start_time + timedelta(minutes=state.step.stepTime)
+        LOGGER.info(f'Starting timer duration {state.step.stepTime} for step {state.step.name}, expected end time: {state.step_end_time}')
+        loop = asyncio.get_event_loop()
+        loop.call_later(state.step.stepTime*60, asyncio.create_task, self.end_timer())
+        await self.datastore_client.store_state(state)
+
+    async def end_timer(self):
+        LOGGER.info('Timer is over, proceeding to next step')
+        await self.proceed_to_next_step()
+
     async def spark_blocks_changed(self, blocks):
-        configuration = await self.datastore_client.load_configuration()
-        step = configuration.current_state.step
-        try:
-            mash_step = configuration.mash.steps[step]
-            expected_temp = mash_step.stepTemp
-            temp_device_block = next((block for block in blocks if block['id'] == self.setpoint_device.id))
-            updated_temp = temp_device_block['data']['value']['value']
-            LOGGER.info(f'--> updated_temp: {updated_temp}, expected: {expected_temp}')
-            if updated_temp >= expected_temp:
-                await self.proceed_to_next_step()
-        except IndexError:
-            LOGGER.warn('attempting to reach mash step while it does not exist')
+        state = await self.datastore_client.load_state()
+        step = state.step
+        if state.automation_state == AutomationState.HEAT:
+            try:
+                expected_temp = step.stepTemp
+                temp_device_block = next((block for block in blocks if block['id'] == self.settings.mashAutomation.setpointDevice.id))
+                updated_temp = temp_device_block['data']['value']['value']
+                LOGGER.info(f'--> updated_temp: {updated_temp}, expected: {expected_temp}')
+                if updated_temp >= expected_temp:
+                    await self.start_timer()
+            except IndexError:
+                LOGGER.warn('attempting to reach mash step while it does not exist')
 
 
 @docs(
--- brewblox_brewfather_service/schemas.py
+++ brewblox_brewfather_service/schemas.py
@@ -3,9 +3,76 @@ Schemas used
 """
 
 import json
-from marshmallow import Schema, ValidationError, fields, post_load
+from enum import Enum
+from datetime import datetime, timedelta
+from marshmallow_enum import EnumField
+from marshmallow import Schema, ValidationError, fields, post_load, EXCLUDE
 from marshmallow.validate import OneOf
-from brewblox_brewfather_service import datastore
+
+
+class AutomationType(Enum):
+    MASH = 1
+    BOIL = 2
+    SPARGE = 3
+    FERMENTATION = 4
+
+
+class AutomationState(Enum):
+    HEAT = 1
+    REST = 2
+
+
+class Device:
+    def __init__(self, service_id: str, id: str):
+        self.service_id = service_id
+        self.id = id
+
+
+class MashStep:
+    def __init__(self, stepTemp, rampTime, stepTime, type, name, displayStepTemp):
+        self.stepTemp = stepTemp
+        self.rampTime = rampTime
+        self.stepTime = stepTime
+        self.type = type
+        self.name = name
+        self.displayStepTemp = displayStepTemp
+
+    def __repr__(self):
+        return '<MashStep(name={self.name!r})>'.format(self=self)
+
+
+class Mash:
+    def __init__(self, name, steps, _id):
+        self.name = name
+        self.steps = steps
+        self._id = _id
+
+    def __repr__(self):
+        return f'<Mash(name={self.name!r}, steps={self.steps!r})>'
+
+
+class CurrentState:
+    def __init__(self, automation_type: AutomationType,
+                 step_index: int, step: MashStep, step_start_time=datetime.utcnow(), step_end_time=datetime.utcnow(), automation_state: AutomationState = AutomationState.REST):
+        self.automation_type = automation_type
+        self.automation_state = automation_state
+        self.step_index = step_index
+        self.step = step
+        self.step_start_time = step_start_time
+        self.step_end_time = step_end_time
+
+    def __repr__(self):
+        return f'<CurrentState(automation_type={self.automation_type!r}, automation_state={self.automation_state!r}, step_index= {self.step_index!r}, step={self.step!r}, step_start_time={self.step_start_time!r}, step_end_time={self.step_end_time!r})>'
+
+
+class MashAutomation:
+    def __init__(self, setpointDevice: Device):
+        self.setpointDevice = setpointDevice
+
+
+class Settings:
+    def __init__(self, mashAutomation: MashAutomation):
+        self.mashAutomation = mashAutomation
 
 
 def validate(schema: Schema, data: dict):
@@ -16,70 +83,67 @@ def validate(schema: Schema, data: dict):
 
 
 class DeviceSchema(Schema):
-    service_id = fields.String(Required=True)
-    id = fields.String(Required=True)
+    service_id = fields.String(required=True)
+    id = fields.String(required=True)
 
     @post_load
     def make_device(self, data, **kwargs):
-        return datastore.Device(**data)
+        return Device(**data)
 
 
 class MashSettingsSchema(Schema):
-    setpointDevice = fields.Nested(DeviceSchema, Required=True)
+    setpointDevice = fields.Nested(DeviceSchema, required=True)
 
     @post_load
     def make_mashautomation_settings(self, data, **kwargs):
-        return datastore.MashAutomation(**data)
+        return MashAutomation(**data)
 
 
 class SettingsSchema(Schema):
-    mashAutomation = fields.Nested(MashSettingsSchema, Required=True)
+    mashAutomation = fields.Nested(MashSettingsSchema, required=True)
 
     @post_load
     def make_settings(self, data, **kwargs):
-        return datastore.Settings(**data)
+        return Settings(**data)
 
 
 class MashStepSchema(Schema):
-    stepTemp = fields.Int(Required=True)
-    rampTime = fields.Raw(Required=True, allow_none=True)
-    stepTime = fields.Int(Required=True)
-    type = fields.String(Required=True, validate=OneOf(['Temperature']))
-    name = fields.String(Required=True)
-    displayStepTemp = fields.Int(Required=True)
+    stepTemp = fields.Int(required=True)
+    rampTime = fields.Raw(required=True, allow_none=True)
+    stepTime = fields.Int(required=True)
+    type = fields.String(required=True, validate=OneOf(['Temperature']))
+    name = fields.String(required=True)
+    displayStepTemp = fields.Int(required=True)
 
     @post_load
     def make_mash_step(self, data, **kwargs):
-        return datastore.MashStep(**data)
+        return MashStep(**data)
 
 
 class MashSchema(Schema):
-    name = fields.String(Required=True)
-    steps = fields.Nested(MashStepSchema, many=True, Required=True)
-    _id = fields.String(Required=True)
+    class Meta:
+        unknown = EXCLUDE
+
+    name = fields.String(required=True)
+    steps = fields.Nested(MashStepSchema, many=True, required=True)
+    _id = fields.String(required=True)
 
     @post_load
     def make_mash(self, data, **kwargs):
-        return datastore.Mash(**data)
+        return Mash(**data)
 
 
 class CurrentStateSchema(Schema):
-    automation = fields.String(Required=True, validate=OneOf(['mash', 'boil', 'sparge', 'fermentation']))
-    step = fields.Int(Required=True)
+    automation_type = EnumField(AutomationType, required=True)
+    automation_state = EnumField(AutomationState, required=True)
+    step_index = fields.Int(required=True)
+    step = fields.Nested(MashStepSchema, allow_none=True, allow_null=True)
+    step_start_time = fields.DateTime(allow_none=True, allow_null=True)
+    step_end_time = fields.DateTime(allow_none=True, allow_null=True)
 
     @post_load
     def make_current_state(self, data, **kwargs):
-        return datastore.CurrentState(**data)
-
-
-class ConfigurationDatastoreSchema(Schema):
-    settings = fields.Nested(SettingsSchema, Required=True)
-    current_state = fields.Nested(CurrentStateSchema, Required=True)
-    mash = fields.Nested(MashSchema, Required=False)
-
-    @post_load
-    def make_configuration(self, data, **kwargs):
-        return datastore.ConfigurationDatastore(**data)
+        return CurrentState(**data)
 
 
 if __name__ == '__main__':
